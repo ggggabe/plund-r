52695bc8ac9139c59e1e3381b0859411400bc62d
Gabe Marquez <crumplepunch@crumplepunch.local>
2016-04-09 20:20:01 -0400

    toggling happens

--- eventPage.js
+++ eventPage.js
@@ -1,6 +1,27 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    console.log(tab.url.search('reddit'));
     if( tab.url.search('reddit') < 0) {
         chrome.tabs.create({ url: "https://www.reddit.com" });
+        chrome.storage.sync.set({'active' : true}, function() {
+            console.log('plund\/r\/ on');
+        });
+    }
+    else {
+        chrome.storage.sync.get('active',
+            function(obj){
+                if(obj.active) {
+                    chrome.storage.sync.set({'active' : false}, function() {
+                        console.log('toggled off');
+
+                    });
+                }
+                else {
+                    chrome.storage.sync.set({'active' : true}, function() {
+                        console.log('toggled on');
+
+                    });
+                }
+            }
+        );
     }
 });
+
