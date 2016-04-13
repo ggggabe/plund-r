1fb0f048ee0498322da828b01e01e9a7ddd265f2
gabe <gabe@gabemarquez.rocks>
2016-04-13 19:52:50 -0400

    tally ho

--- hunt.js
+++ hunt.js
@@ -1,6 +1,6 @@
 function arrYeReady() {
     $('body').css('overflow','hidden');
-    $('body').prepend("<div class='plundr'><h2 id='plundr-message' style='color:#fff !important'>Thar be <span class='gold'>gold</span> up ahead, /r/ ye ready?</h2><h2 id='timer'>5</h2><h2>Click the gold on a gilded comment to collect yer booty!</h2></div>");
+    $('body').prepend("<div class='plundr'><h2 id='plundr-message' style='color:#fff !important'>Thar be <span class='gold'>gold</span> up ahead, /r/ ye ready?</h2><h2 id='timer'>Do ye smell that?</h2><h2>Click the gold on a gilded comment to collect yer booty!</h2></div>");
 
     $('body').append('<div style="display:block;height:0;width:0;visibility:hidden;overflow:hidden;"><iframe width="420" height="315" src="https://www.youtube.com/embed/27mB8verLK8?autoplay=1" frameborder="0" allowfullscreen></iframe></div>');
 }
@@ -25,21 +25,24 @@ function yerLookinFerGold() {
 }
 
 function tick(){
-    var seconds = 6;
+    var seconds = 3;
     var timer = setInterval(function() {
         seconds--;
-        if( seconds > 0) {
-            $('#timer').html(seconds);
+        if( seconds == 2) {
+            $('#timer').html("Do ye smell that?");
+        }
+        else if( seconds == 1) {
+            $('#timer').html("The smell o' gold?");
         }   
         else if( seconds == 0 ) {
-            $('#timer').html("YAR!");
+            $('#timer').html("TALLY HO!");
         }
         else{
             $('body').css('overflow','');
             $('.plundr').css('display','none');
             clearInterval(timer);
         }
-    }, 1000);
+    }, 1500);
 
 }
 
