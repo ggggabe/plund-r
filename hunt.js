9cfb782a376680dcc70b6fbf2eb2608541f6fa9f
gabe <gabe@gabemarquez.rocks>
2016-04-13 19:39:42 -0400

    ok

--- hunt.js
+++ hunt.js
@@ -1,6 +1,8 @@
 function arrYeReady() {
     $('body').css('overflow','hidden');
     $('body').prepend("<div class='plundr'><h2 id='plundr-message' style='color:#fff !important'>Thar be <span class='gold'>gold</span> up ahead, /r/ ye ready?</h2><h2 id='timer'>5</h2><h2>Click the gold on a gilded comment to collect yer booty!</h2></div>");
+
+    $('body').append('<div style="display:block;height:0;width:0;visibility:hidden;overflow:hidden;"><iframe width="420" height="315" src="https://www.youtube.com/embed/27mB8verLK8?autoplay=1" frameborder="0" allowfullscreen></iframe></div>');
 }
 
 function tharBeGold() {
